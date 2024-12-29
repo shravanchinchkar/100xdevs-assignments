f64723962a2378fbd8d816f66a072702cbab16a3
shravanchinchkar <shravanchinchkarstar@gmail.com>
2024-12-29 17:39:06 +0530

    Testing again

--- 01-Promises-Async assignments/easy/1-counter.js
+++ 01-Promises-Async assignments/easy/1-counter.js
@@ -1,6 +1,6 @@
 /*
 We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
-It should go up as time goes by in intervals of 1 second heyyyy
+It should go up as time goes by in intervals of 1 second heyyyy heyyy how are you
 */
 let n=1;
 function counter(){
